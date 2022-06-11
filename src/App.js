e13df367b6798f51f2ca8a4e45398e5abc5177a9
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2022-06-11 17:35:51 +0600

    added the firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './categories.styles.scss';
+import Home from "./routes/home/home.component";
+import {Route, Routes, Outlet } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const Shop = ()=>{
+
+    return (
+        <h1>Shop page</h1>
+    )
 }
+
+
+
+const App = () => {
+
+    return (
+
+        <Routes>
+            <Route path="/" element={<Navigation/>}>
+                <Route index element={<Home/>}/>
+                <Route path="shop" element={<Shop/>}/>
+                <Route path="auth" element={<Authentication/>}/>
+            </Route>
+        </Routes>
+
+    )
+};
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,90 @@
+import {initializeApp} from "firebase/app";
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from 'firebase/auth';
+
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfi5BqNJ34czpGv5M3Tor_9xBVhWUO0Mc",
+    authDomain: "shoppingapp-6bd5b.firebaseapp.com",
+    projectId: "shoppingapp-6bd5b",
+    storageBucket: "shoppingapp-6bd5b.appspot.com",
+    messagingSenderId: "928465913178",
+    appId: "1:928465913178:web:2efe00657d7823a1421f63"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account"
+})
+
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+
+export const db = getFirestore();
+
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
+
+    if(!userAuth) return;
+
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapShot = await getDoc(userDocRef);
+
+    if (!userSnapShot.exists()) {
+
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
+
+
+        try {
+
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt,
+                ...additionalInformation
+            })
+        } catch (error) {
+            console.log("error creating the user", error.message)
+        }
+
+    }
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+}
+
+
+
+
