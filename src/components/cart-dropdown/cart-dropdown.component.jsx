b2956498d3a45912b576011f971e1ce4bea53706
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2022-06-12 13:35:57 +0600

    completed the cart items page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,30 @@
 import './cart-dropdown.styles.scss';
 
-
-import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import React, {useContext} from 'react';
 import Button from "../button/button.component";
+import {CartContext} from "../../context/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
+
+    const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+
+    const gotoCheckoutHandler = ()=>{
+        navigate("/checkout");
+    }
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button>GO TO CHECKOUT</Button>
+            <div className='cart-items'>
+                {cartItems.map(item=> (
+
+                    <CartItem key={item.id} cartItem={item}/>
+                ))}
+            </div>
+            <Button onClick={gotoCheckoutHandler}>CHECKOUT</Button>
         </div>
     );
 };
--- src/components/product-cart/product-card.component.jsx
+++ src/components/product-cart/product-card.component.jsx
@@ -1,14 +1,18 @@
 import './product-card.styles.scss';
 
 
-import React from 'react';
+import React, {useContext} from 'react';
 import Button from "../button/button.component";
+import {CartContext} from "../../context/cart.context";
 
 
 
 const ProductCard = ({product}) => {
 
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+
+    const addProductToCart = ()=> addItemToCart(product);
 
 
     return (
@@ -18,7 +22,7 @@ const ProductCard = ({product}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted'  onClick={addProductToCart}>Add to card</Button>
         </div>
     );
 };
