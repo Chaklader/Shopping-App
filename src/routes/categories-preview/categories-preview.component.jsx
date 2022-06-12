b291760e0163ce3bd16226bd10cf491e05a2e536
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2022-06-12 20:25:22 +0600

    working code

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,12 +1,7 @@
 import React, {Fragment, useContext} from 'react';
 
-import SHOP_DATA from '../../shop-data.json';
 import {CategoriesContext} from "../../context/categories.context";
-import ProductCard from "../../components/product-cart/product-card.component";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
-
-
-// import './categories-preview.styles.scss';
 
 
 const CategoriesPreview = () => {
@@ -14,19 +9,6 @@ const CategoriesPreview = () => {
     const {categoriesMap} = useContext(CategoriesContext);
 
     return (
-        // <div className='shop-container'>
-        //     {Object.keys(categoriesMap).map((title) => (
-        //             <Fragment key={title}>
-        //                 <h2>{title}</h2>
-        //                 <div className="products-container">
-        //                     {categoriesMap[title].map((product) => (
-        //                         <ProductCard key={product.id} product={product}/>
-        //                     ))}
-        //                 </div>
-        //             </Fragment>
-        //         ))}
-        // </div>
-
         <>
             {Object.keys(categoriesMap).map((title) => {
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,43 +1,19 @@
 import React, {Fragment, useContext} from 'react';
 
-import SHOP_DATA from '../../shop-data.json';
-import {CategoriesContext} from "../../context/categories.context";
-import ProductCard from "../../components/product-cart/product-card.component";
-import CategoryPreview from "../categories-preview/categories-preview.component";
-
 
 import './shop.styles.scss';
 import {Route, Routes} from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 
 const Shop = () => {
 
-    // const {categoriesMap} = useContext(CategoriesContext);
 
     return (
-        // <div className='shop-container'>
-        //     {Object.keys(categoriesMap).map((title) => (
-        //             <Fragment key={title}>
-        //                 <h2>{title}</h2>
-        //                 <div className="products-container">
-        //                     {categoriesMap[title].map((product) => (
-        //                         <ProductCard key={product.id} product={product}/>
-        //                     ))}
-        //                 </div>
-        //             </Fragment>
-        //         ))}
-        // </div>
-
-        // <div className='shop-container'>
-        //     {Object.keys(categoriesMap).map((title) => {
-        //
-        //         const products = categoriesMap[title];
-        //         return <CategoryPreview key={title} title={title} products={products}/>
-        //     })}
-        // </div>
-
         <Routes>
-            <Route index element={<CategoryPreview/>}/>
+            <Route index element={<CategoriesPreview/>}/>
+            <Route path=":category" element={<Category/>}/>
         </Routes>
     );
 };
