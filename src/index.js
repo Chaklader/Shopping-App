65a1a9d7a0640fd7ddcf1b5d00eff15af507484e
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2022-06-15 19:44:15 +0600

    created the redux files

--- src/index.js
+++ src/index.js
@@ -11,29 +11,14 @@ import {CartProvider} from "./context/cart.context";
 
 /*
 
-- store folder -> store.js
-- root-reducer.js
-- store->user-> user.reducer.js
 - index.js
 - App.js
-- user.action.js
-- user.types.js
 - navigation.component.jsx
-- user.selector.js
-- categories-> categories.reducer.js
-- category.action.js
-- category.type.js
-- category.selector.js
-- shop.js
 - Delete the CategoriesProvider from the codebase
 - CategoriesPreview
 - category.component.jsx
 - delete categories.context.jsx
 - firebase.utils.js
-- cart-> cart.action.js
-- cart.reducer.js
-- cart.selector.js
-- cart.type.js
 
 
 
